b8f7f33646270b41ddb507d7315e95c6c2166ccf
RamyQ <116669373+RamyQ@users.noreply.github.com>
2025-04-09 15:57:52 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UserPage from './components/UserPage';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <UserPage/>
+    </div>
+  );
+}
+
+export default App;
